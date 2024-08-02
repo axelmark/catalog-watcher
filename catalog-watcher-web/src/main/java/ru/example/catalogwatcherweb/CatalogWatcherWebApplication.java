0c9aebba64f0baaf7a1a939c955e6e231d54bf06
axelmark <axelmark@outlook.com>
2024-08-02 23:15:40 +0300

    Some updates

--- catalog-watcher-web/src/main/java/ru/example/catalogwatcherweb/CatalogWatcherWebApplication.java
+++ catalog-watcher-web/src/main/java/ru/example/catalogwatcherweb/CatalogWatcherWebApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CatalogWatcherWebApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CatalogWatcherWebApplication.class, args);
     }
-
 }
